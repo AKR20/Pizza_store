0bf8ab367c299fe55df8326ca53cddc064dc9c98
Akshay Rahul <akshayrahul92@gmail.com>
2023-04-18 22:03:39 +0530

    modiified

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -2,7 +2,8 @@ import React from 'react';
 import './Header.css';
 import logo from './logo.png';
 import { FaShoppingCart } from 'react-icons/fa';
-import './Cart.js'
+import './Cart.js';
+import { Link } from 'react-router-dom';
 
 function Header() {
   return (
@@ -13,10 +14,10 @@ function Header() {
           <h1 className="site-title">PizzaLicious</h1>
         </div>
         <ul className="nav-links">
-          <li><a href="#" className="nav-link">Home</a></li>
+          <li><Link to="/" href="#" className="nav-link">Home</Link></li>
           <li><a href="#" className="nav-link">About</a></li>
-          <li><a href="/cart" className="nav-link contact-link">Contact</a></li>
-          <li><a href="#" className="nav-link cart-icon"><FaShoppingCart /></a></li>
+          <li><a href="#" className="nav-link contact-link">Contact</a></li>
+          <li><Link to="/Cart" className="nav-link cart-icon"><FaShoppingCart /></Link></li>
         </ul>
       </nav>
     </div>
